34e806e00a2b752eae6e0d585c62e0ffb301d8e0
Philip Weiss <philip@weiss.com>
2016-08-21 23:31:15 -0700

    Keyboard interface function prototypes

--- src/includes/KeyboardAPI.h
+++ src/includes/KeyboardAPI.h
@@ -8,7 +8,11 @@
  *      
  * http://flyingcarsandstuff.com/projects/NanoKeys/
  *
+ * 
+ * 	Modified on: Aug 21, 2016
+ * 		Author: Philip Weiss (dude8604)
  *
+ * 
  * This program library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -56,6 +60,13 @@ public:
 	virtual ~Keyboard();
 
 	virtual size_t write(uint8_t c);
+	
+	/* dude8604 */
+	
+	virtual bool pressKey(uint8_t keyCode);
+	virtual void releaseKey();
+			
+	/* end dude8604 */
 
 };
 
